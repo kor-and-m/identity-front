414dfeb31824dfde43e2b74eb04b4b4ba4a277df
andrey <gussman7777@gmail.com>
2018-05-22 02:22:23 +0300

    update

--- src/app/cabinet/ubdate-scope/ubdate-scope.component.ts
+++ src/app/cabinet/ubdate-scope/ubdate-scope.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-ubdate-scope',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UbdateScopeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	console.log(this.route);
   }
 
 }
